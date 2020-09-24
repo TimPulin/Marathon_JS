cf665a72c760f9384f070b575a1187a282d8787c
TimPulin <kyliki@yandex.ru>
2020-09-24 23:02:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
-
-
-
 const firstRow = "мама мыла раму";
 const secondRow = "собака друг человека";
-
-getRow();
 
 function getRow(){
     let amountOfA, amountFirstrow, amountSecondrow;
 
-    CountA(firstRow);
-    amountFirstrow=amountOfA;
-    CountA(secondRow)
-    amountSecondrow=amountOfA;
-
-    if (amountFirstrow>amountSecondrow){
-       return firstRow;
-    }
-    else {
-       return secondRow;
-    }
+    return (CountA(firstRow) > CountA(secondRow)) ? firstRow : secondRow ;
 
     function CountA(string) {
-       amountOfA=0;
-        for(let i=0; i<string.length; i++){
+        amountOfA = 0;
+        for(let i = 0; i<string.length; i++){
             if(string.charAt(i) === "а"){
                 ++amountOfA;
             }
-      }
+        } return amountOfA;
     }
 }
 
-console.log(getRow(firstRow, secondRow))
+console.log(getRow(firstRow, secondRow));
